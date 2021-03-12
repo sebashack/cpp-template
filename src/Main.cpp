709c679563907a40ba28fd0065f4fbc8cf794e77
sebashack <sebastian0092@gmail.com>
2021-03-12 12:38:26 -0500

    Some pointer examples

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,11 @@
 #include "Foo.h"
 #include "Bar.cpp"
 
+void printChar(char& c)
+{
+    std::cout << c << std::endl;
+}
+
 int main()
 {
     Foo foo;
@@ -10,6 +15,13 @@ int main()
     Bar bar;
     bar.str = "boooo !!!!";
     bar.do_something();
+
+    char v[6] = {'a', 'b', 'c', 'd','e', 'f'};
+    char* p = &v[3];
+
+
+    std::cout << "Both are the same: " << p[1] << " " << *(p + 1) << std::endl;
+    printChar(p[1]);
 
     return 0;
 }
