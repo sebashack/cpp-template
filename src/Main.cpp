e62426bd601c71762dbed78a59f0c18307323e93
sebashack <sebastian0092@gmail.com>
2021-04-10 18:06:41 -0500

    Partial main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,37 +1,26 @@
+#include <time.h>
+
 #include "DoubleLinkedList.h"
 #include "Word.h"
 
 
 int main()
 {
-    // DoubleLinkedList<Word> ls;
-
-    Word tk0("hola", Subject, 1);
-    Word tk1("mundo", Subject, 1);
-    Word tk2("como", Subject, 1);
-    Word tk3("estas", Subject, 1);
-
-    // tk0.increaseFrequency();
-
-    // ls.insertSorted(tk0);
-    // ls.insertSorted(tk1);
-    // ls.insertSorted(tk2);
-
-    // for (Word tk : ls)
-    // {
-    //     std::cout << tk << std::endl;
-    // }
-
-    // std::cout << ls.length() << std::endl;
-
     DoubleLinkedList<Word> words;
-    readWords("/home/sebastian/university/programming_languages/cpp_template/words.txt", words);
-    //DoubleLinkedList<Word> words;
-    //std::cout << words.length() << std::endl;
+    word_count count = readWords("/home/sebastian/university/programming_languages/cpp_template/sample-words.txt", words);
 
-    for (Word w : words)
+
+    short generatedSentences = 0;
+    words.shuffle();
+
+    while (generatedSentences <= MAX_WORDS && words.length() > 0)
     {
-        std::cout << w << std::endl;
+        std::cerr << generatedSentences + 1 << ") ";
+        processWord(words, count, Subject);
+        processWord(words, count, Verb);
+        processWord(words, count, Predicate);
+
+        ++generatedSentences;
     }
 
     return 0;
